d3b9fff78754066cc0d67392db26563d6d0268c7
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-03-11 11:37:49 -0600

    update main activity

--- app/src/main/java/com/desapp/earthquakes/MainActivity.java
+++ app/src/main/java/com/desapp/earthquakes/MainActivity.java
@@ -3,29 +3,39 @@ package com.desapp.earthquakes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.desapp.earthquakes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    //ActivityMainBinding binding;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
+        binding.eqRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        EqAdapter adapter = new EqAdapter();
+        adapter.setOnItemClickListener(earthquake ->
+                Toast.makeText(MainActivity.this,
+                        earthquake.getPlace(),
+                        Toast.LENGTH_SHORT).show());
+        binding.eqRecycler.setAdapter(adapter);
+
         viewModel.getEqList().observe(this, eqList -> {
-            for (Earthquake eq: eqList) {
-                Log.d("eq", eq.getMagnitude() + " " + eq.getPlace());
-            }
+            adapter.submitList(eqList);
         });
+
         viewModel.getEarthquakes();
     }
 }
