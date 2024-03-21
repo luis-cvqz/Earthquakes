ef40abef537373255f1e426e92c1d6284dbb5d3d
Luis C <zs21013875@estudiantes.uv.mx>
2024-03-21 14:17:52 -0600

    Code cleanup

--- app/src/main/java/com/desapp/earthquakes/MainRepository.java
+++ app/src/main/java/com/desapp/earthquakes/MainRepository.java
@@ -1,7 +1,5 @@
 package com.desapp.earthquakes;
 
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
 
 import com.desapp.Earthquake;
@@ -12,6 +10,7 @@ import com.desapp.database.EarthquakeDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -22,10 +21,6 @@ public class MainRepository {
 
     public MainRepository(EarthquakeDatabase database) {
         this.database = database;
-    }
-
-    public interface  DownloadEqsListener {
-        void onEqsDownloaded(List<Earthquake> eqList);
     }
 
     public LiveData<List<Earthquake>> getEqList() {
@@ -70,20 +65,5 @@ public class MainRepository {
         }
 
         return eqList;
-    }
-
-    public void getEarthquakes(DownloadEqsListener downloadEqsListener) {
-        ApiClient.Service service = ApiClient.getInstance().getService();
-        service.getEarthquakes().enqueue(new Callback<EarthquakeJSONResponse>() {
-            @Override
-            public void onResponse(Call<EarthquakeJSONResponse> call, Response<EarthquakeJSONResponse> response) {
-                Log.d("rep", response.body().toString());
-                List<Earthquake> earthquakeList = getEarthquakesWithMoshi(response.body());
-                downloadEqsListener.onEqsDownloaded(earthquakeList);
-            }
-
-            @Override
-            public void onFailure(Call<EarthquakeJSONResponse> call, Throwable t) { }
-        });
     }
 }
--- app/src/main/java/com/desapp/earthquakes/MainViewModel.java
+++ app/src/main/java/com/desapp/earthquakes/MainViewModel.java
@@ -5,23 +5,15 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.desapp.Earthquake;
 import com.desapp.database.EarthquakeDatabase;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainViewModel extends AndroidViewModel {
 
     private final MainRepository repository;
-
-    private final MutableLiveData<List<Earthquake>> eqList = new MutableLiveData<>();
 
     public MainViewModel(@NonNull Application application) {
         super(application);
@@ -35,54 +27,5 @@ public class MainViewModel extends AndroidViewModel {
 
     public void downloadEarthquakes() {
         repository.downloadAndSaveEarthquakes();
-    }
-
-    public void getEarthquakes() {
-        repository.getEarthquakes(earthquakeList -> {
-            eqList.setValue(earthquakeList);
-        });
-    }
-
-    /*
-    public void  getEarthquakes() {
-        ApiClient.Service service = ApiClient.getInstance().getService();
-
-        service.getEarthquakes().enqueue(new Callback<EarthquakeJSONResponse>() {
-            @Override
-            public void onResponse(Call<EarthquakeJSONResponse> call, Response<EarthquakeJSONResponse> response) {
-                List<Earthquake> eql = getEarthquakesWithMosh(response.body());
-                eqList.setValue(eql);
-            }
-
-            @Override
-            public void onFailure(Call<EarthquakeJSONResponse> call, Throwable t) { }
-        });
-    }*/
-
-    private List<Earthquake> parseEarthquake(String body) {
-        ArrayList<Earthquake> eql = new ArrayList<>();
-        try {
-            JSONObject jsonResponse = new JSONObject(body);
-            JSONArray features = jsonResponse.getJSONArray("features");
-            for (int i = 0; i < features.length(); i++) {
-                JSONObject jsonFeature = features.getJSONObject(i);
-                String id = jsonFeature.getString("id");
-
-                JSONObject jsonProperties = jsonFeature.getJSONObject("properties");
-                double mag = jsonProperties.getDouble("mag");
-                String place = jsonProperties.getString("place");
-                long time = jsonProperties.getLong("time");
-
-                JSONObject jsonGeometry = jsonFeature.getJSONObject("geometry");
-                JSONArray coords = jsonGeometry.getJSONArray("coordinates");
-                double longitude = coords.getDouble(0);
-                double latitude = coords.getDouble(1);
-
-                eql.add(new Earthquake(id, place, mag, time, longitude, latitude));
-            }
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-        return eql;
     }
 }
