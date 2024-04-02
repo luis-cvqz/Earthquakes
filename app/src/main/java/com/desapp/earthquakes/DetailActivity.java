4ef3b0b04d4bef7dd5e799263370b86d9b4c8d18
Luis C <zs21013875@estudiantes.uv.mx>
2024-04-01 20:02:50 -0600

    MapView logic

--- app/src/main/java/com/desapp/earthquakes/DetailActivity.java
+++ app/src/main/java/com/desapp/earthquakes/DetailActivity.java
@@ -1,17 +1,27 @@
 package com.desapp.earthquakes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.desapp.Earthquake;
 import com.desapp.earthquakes.databinding.ActivityDetailBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback {
     public static final String EARTHQUAKE_KEY = "earthquake_key";
 
     ActivityDetailBinding binding;
 
+    private MapView mMapView;
+    private GoogleMap mMap;
+    private LatLng earthquakeCoordinates;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,22 @@ public class DetailActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         Earthquake earthquake = extras.getParcelable(EARTHQUAKE_KEY);
 
+        earthquakeCoordinates = new LatLng(earthquake.getLatitude(), earthquake.getLongitude());
+
+        mMapView = binding.mapView;
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+
         binding.setEarthquake(earthquake);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        if (mMapView != null) {
+            MarkerOptions marker = new MarkerOptions().position(earthquakeCoordinates).title("Earthquake");
+            mMap.addMarker(marker);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(earthquakeCoordinates, 4));
+        }
     }
 }
